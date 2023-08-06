335aef4d2eb63918d109f2c827b0486f556e3779
Max Chou <yihsunmaxchou@icloud.com>
2023-08-06 04:34:24 -0700

    Added `postcss-css-variables`

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
     ['postcss-preset-env', {}],
     require('postcss-import'),
     require('@csstools/postcss-design-tokens'),
+    require('postcss-css-variables'),
     require('postcss-nesting'),
     require('postcss-each'),
     require('cssnano')
